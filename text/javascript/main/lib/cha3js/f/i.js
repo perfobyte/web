6f6173a18a0e9d221402c781cb1043e33ab0af0a
vladprosvirin <vladprosvirin@gmail.com>
2025-12-09 02:55:31 +0100

    a

--- text/javascript/main/lib/cha3js/f/i.js
+++
@@ -1,5 +0,0 @@
-export {default as c3_to32} from './to32.js';
-export {default as c3_update} from './update.js';
-export {default as c3_param} from './param.js';
-export {default as c3_param_setup} from "./param_setup.js";
-export {default as c3_reload} from './reload.js';
