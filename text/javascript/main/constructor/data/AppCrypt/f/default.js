eb57070324208d9d0eac812638f0cd89e9bd388a
vladprosvirin <vladprosvirin@gmail.com>
2026-01-26 05:13:41 +0100

    a

--- text/javascript/main/constructor/data/AppCrypt/f/default.js
+++ text/javascript/main/constructor/data/AppCrypt/f/default.js
@@ -3,40 +3,38 @@
 
 export default (
     (
-        (
-            AppCrypt,
+        AppCrypt,
 
-            Uint8Array,
-            Uint32Array,
-            ArrayFrom,
-            DataView
-        ) => {
-            var
-                byte_counter = 0,
-                rounds = 20,
-                block_size = 64,
-                key_length = 32,
-                nonce_length = 12,
-                param_length = 16,
-        
-                length = 4
-            ;
-            return (
-                new AppCrypt(
-                    byte_counter,
-                    rounds,
-                    block_size,
-                    key_length,
-                    nonce_length,
-                    param_length,
-                    length,
+        Uint8Array,
+        Uint32Array,
+        ArrayFrom,
+        DataView
+    ) => {
+        var
+            byte_counter = 0,
+            rounds = 20,
+            block_size = 64,
+            key_length = 32,
+            nonce_length = 12,
+            param_length = 16,
+    
+            length = 4
+        ;
+        return (
+            new AppCrypt(
+                byte_counter,
+                rounds,
+                block_size,
+                key_length,
+                nonce_length,
+                param_length,
+                length,
 
-                    Uint8Array,
-                    Uint32Array,
-                    ArrayFrom,
-                    DataView
-                )
+                Uint8Array,
+                Uint32Array,
+                ArrayFrom,
+                DataView
             )
-        }
-    )
+        )
+    }
 );
